9424832b36331f4b149c9ed7f8d28b0ed82b2b37
behrooz pahlavan <pahlavan111@gmail.com>
2020-06-23 23:37:36 +0430

    create person Database abstract class

---
+++ app/src/main/java/com/bp/roomtest/PersonDatabase.java
@@ -0,0 +1,10 @@
+package com.bp.roomtest;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Person.class},version = 1,exportSchema = false)
+public abstract class PersonDatabase extends RoomDatabase {
+
+    public abstract PersonDAO personDAO();
+}
