2552db318fdcf8ddb768f5070bd43aabc9057026
behrooz pahlavan <pahlavan111@gmail.com>
2020-06-23 23:26:33 +0430

    create entity class named Person

---
+++ app/src/main/java/com/bp/roomtest/Person.java
@@ -0,0 +1,56 @@
+package com.bp.roomtest;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Person {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @ColumnInfo(name = "family")
+    private String family;
+
+    @ColumnInfo(name = "age")
+    private int age;
+
+    public Person(String name, String family, int age) {
+        this.name = name;
+        this.family = family;
+        this.age = age;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
