87855f842eae1f5f8f5d7cf335cce985d2dd3083
behrooz pahlavan <pahlavan111@gmail.com>
2020-06-27 18:12:15 +0430

    finished edit dialog

--- app/src/main/java/com/bp/roomtest/PersonAdapter.java
+++ app/src/main/java/com/bp/roomtest/PersonAdapter.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -56,7 +57,7 @@ public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.PersonHold
         holder.btn_delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(activity, "delete", Toast.LENGTH_SHORT).show();
+              //  Toast.makeText(activity, "delete", Toast.LENGTH_SHORT).show();
 
                 AlertDialog.Builder builder;
                 builder = new AlertDialog.Builder(activity);
@@ -110,7 +111,86 @@ public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.PersonHold
         holder.btn_edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(activity, "edit", Toast.LENGTH_SHORT).show();
+              //  Toast.makeText(activity, "edit", Toast.LENGTH_SHORT).show();
+
+
+                AlertDialog.Builder builder= new AlertDialog.Builder(activity);
+                final LayoutInflater inflater= LayoutInflater.from(activity);
+                View view= inflater.inflate(R.layout.dialog_edit,null);
+                builder.setView(view);
+                final AlertDialog dialog=builder.create();
+                dialog.show();
+
+                final EditText edt_Name,edt_family,edt_age;
+                Button btn_edit,btn_cancel;
+
+                edt_Name = dialog.findViewById(R.id.edt_name_dialog);
+                edt_family = dialog.findViewById(R.id.edt_family_dialog);
+                edt_age = dialog.findViewById(R.id.edt_age_dialog);
+                btn_edit = dialog.findViewById(R.id.btn_edit_dialog);
+                btn_cancel = dialog.findViewById(R.id.btn_cancel_dialog);
+
+                edt_Name.setText(person.getName());
+                edt_family.setText(person.getFamily());
+                edt_age.setText(String.valueOf(person.getAge()));
+
+                btn_cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                btn_edit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String name = edt_Name.getText().toString();
+                        String family = edt_family.getText().toString();
+                        String age = edt_age.getText().toString();
+
+                        if (name.isEmpty()) {
+                            edt_Name.setError("Enter a name please!");
+                            edt_Name.requestFocus();
+                        }else if (family.isEmpty()) {
+                            edt_family.setError("Enter a family please!!");
+                            edt_family.requestFocus();
+                        }else if (age.isEmpty()) {
+                            edt_age.setError("please enter a right value of age !!!");
+                            edt_age.requestFocus();
+                        }else {
+                            person.setName(name);
+                            person.setFamily(family);
+                            person.setAge(Integer.parseInt(age));
+                            repository.getPersonDatabase().personDAO().updatePerson(person)
+                                    .subscribeOn(Schedulers.io())
+                                    .observeOn(AndroidSchedulers.mainThread())
+                                    .subscribe(new SingleObserver<Integer>() {
+                                        @Override
+                                        public void onSubscribe(Disposable d) {
+
+                                        }
+
+                                        @Override
+                                        public void onSuccess(Integer integer) {
+                                            if (integer==1){
+                                                Toast.makeText(activity, "Person Information updated", Toast.LENGTH_SHORT).show();
+                                                dialog.dismiss();
+                                                notifyDataSetChanged();
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onError(Throwable e) {
+
+                                        }
+                                    });
+                        }
+                    }
+                });
+
+
+
+
             }
         });
 
@@ -137,4 +217,5 @@ public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.PersonHold
             btn_delete = itemView.findViewById(R.id.btn_delete);
         }
     }
+
 }
